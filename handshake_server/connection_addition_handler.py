cad0d82ef5f791d11ab2739a90d8cb7e0f91e735
Vishal Mathur <vishmathur5@gmail.com>
2015-06-28 11:12:49 -0700

    updated the function to add connections

--- handshake_server/connection_addition_handler.py
+++ handshake_server/connection_addition_handler.py
@@ -5,25 +5,23 @@ import json
 
 class ConnectionAdditionHandler(RequestHandler):
 	def post(self):
-		user_id = self.get_argument('user_id')
-		print user_id
-		
+		user_id = self.get_argument('user_id')	
+		friend_id = self.get_argument('friend_id')	
 		con = pycps.Connection('tcp://cloud-us-0.clusterpoint.com:9007', 'users', 'vmmonkey@gmail.com', 'monkey', '100642')
 		try:
-			response = con.retrieve(id)
+			response = con.lookup(user_id)
 		except pycps.APIError as e:
-			print(e)
-			if e.code == 2824:
-				print("Requested non-existing id(s): {0}".format(', '.join(e.document_id)))
+			print 'no data available'
 			return
 		try:
 			if response.get_documents:
 				print("Found {0} documents: ".format(response.found))
-				user = response.get_documents().items()[0]
-
+				user = response.get_documents().items()[0][1]
+				user['connections']+=','+friend_id
+				con.update({user_id: user})
 		except Exception as e:
 			print(e)
 			return
-		print user
-		self.write(json.dumps(user))
+
+		print 'Added connection'
 		self.finish()
