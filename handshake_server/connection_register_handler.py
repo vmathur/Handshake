873728f8f1100f9415abc4debe920e76b36f3b5b
Vishal Mathur <vishmathur5@gmail.com>
2015-06-28 11:58:06 -0700

    more updates to search

--- handshake_server/connection_register_handler.py
+++ handshake_server/connection_register_handler.py
@@ -11,17 +11,18 @@ class ConnectionRegisterHandler(RequestHandler):
 	def post(self):
 		user_id = self.get_argument('user_id')
 		devices = self.get_argument('devices')
-		print 'hello connection'
 		con = pycps.Connection('tcp://cloud-us-0.clusterpoint.com:9007', 'users', 'vmmonkey@gmail.com', 'monkey', '100642')
-
+		print devices
 		responses = []
-
+		devices = devices.split(',')
 		for device in devices:
+			print device
 			try:
-				response = user_db.search(blueooth_address(device))
+				response = con.search(term('1', 'bluetooth_address'))
+				print response
 				responses.append(response)
 			except pycps.APIError as e:
-				print 'test'
+				print 'there was an error'
 		
 		self.write(responses)
 		self.finish()
