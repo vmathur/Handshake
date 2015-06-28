c7010c6d1a75594098bd7a7bed701fbdd329e671
Akhil Batra <akhilbatra@berkeley.edu>
2015-06-28 00:17:19 -0700

    added some profile changes

--- Handshake/app/src/main/java/com/angelhack/handshake/ProfileViewerFragment.java
+++ Handshake/app/src/main/java/com/angelhack/handshake/ProfileViewerFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by AkhilBatra on 6/27/15.
@@ -19,6 +20,16 @@ public class ProfileViewerFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.profile_page, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+//        getActivity().findViewById(R.id.toolbar).setBackgroundColor(0xFFFFFFFF);
+        getActivity().findViewById(R.id.toolbar).setElevation(0);
+        ((TextView)getActivity().findViewById(R.id.profileName)).setText("Paula Barcante");
+        ((TextView)getActivity().findViewById(R.id.profileTagLine)).setText("UX Designer at Amazon");
     }
 
     @Override
